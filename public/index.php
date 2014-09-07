1c3ccde85a458911601830be1cc122c7297e4c73
Dmitrey Kurgannikov <true.masterx2@gmail.com>
2014-09-08 00:38:43 +0400

    Cat

--- public/index.php
+++ public/index.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: masterx2
- * Date: 26.08.14
- * Time: 18:23
- */ 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mimimi</title>
+    <style>
+        #cat {
+            left: 50%;
+            top: 50%;
+            position: absolute;
+            margin-left: -250px;
+            margin-top: -250px;
+        }
+    </style>
+</head>
+<body>
+    <div id="cat">
+        <img src="img/mimimi.jpg" alt=""/>
+    </div>
+</body>
+</html>
